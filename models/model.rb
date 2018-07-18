307da6c6e93c219bb3d9960bd574772c65fbc582
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-18 17:29:50 +0000

    automatically backed up by learn

--- models/model.rb
+++ models/model.rb
@@ -1,5 +1,7 @@
 #this is where our ruby goes
 def get_animal (animal, hat)
-  pet_img = {:dogs = []
-             :hats = []
-            }
+  if animal == "dog"
+      animal_img = ""
+    if hat == "party"
+    elsif hat == ""
+  elsif animal == "cat"
