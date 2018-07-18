7ca78b2373044e9e1c1c2ef1a47a4503df0db154
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-18 18:02:32 +0000

    automatically backed up by learn

--- app_contrlr.rb
+++ app_contrlr.rb
@@ -2,10 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/food_form' do
-    erb :food_form
+  get '/' do
+    erb :index
   end
 
   # Add your post route and action below
+  post '/' do
+    erb :results
 
 end
