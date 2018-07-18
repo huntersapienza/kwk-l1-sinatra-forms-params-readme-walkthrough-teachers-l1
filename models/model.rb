c26a3b266fb3700d820bf82a384196e6f88d7ba0
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-18 17:30:24 +0000

    automatically backed up by learn

--- models/model.rb
+++ models/model.rb
@@ -3,5 +3,7 @@ def get_animal (animal, hat)
   if animal == "dog"
       animal_img = ""
     if hat == "party"
-    elsif hat == ""
+      animal_img = ""
+    elsif hat == "top"
+      animal_img = ""
   elsif animal == "cat"
